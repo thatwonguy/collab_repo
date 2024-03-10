9ccdc8186e6ab2fb1f6451ff9ba25fcf84df5542
thatwonguy <monsore1337@gmail.com>
2024-03-09 21:35:01 -0800

    i am adding a javascript code that we can build on, initial commit

---
+++ js_gui.js
@@ -0,0 +1,11 @@
+const { App } = require('@nodegui/react-nodegui')
+
+function App() {
+    return (
+        <Window title="My Gui (graphical user interface) App">
+            <Text>"Hello My Comp Sci Buddy!"</Text>
+        </Window>
+    );
+}
+
+App.main();
