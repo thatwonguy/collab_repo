3a8c9e81ff839069a4eaf76d26166d5f3eff6478
Lemar <lemar.saffi@gmail.com>
2024-03-09 21:49:13 -0800

    modified text

--- js_gui.js
+++ js_gui.js
@@ -3,7 +3,7 @@ const { App } = require('@nodegui/react-nodegui')
 function App() {
     return (
         <Window title="My Gui (graphical user interface) App">
-            <Text>"Hello My Comp Sci Buddy!"</Text>
+            <Text>"Hello!"</Text>
         </Window>
     );
 }
